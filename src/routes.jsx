b0ea7ffd6a05810d4cff9f7d504a580a0c98aafe
Duy <duypersonal512@gmail.com>
2025-11-06 15:47:49 -0600

    Add signup page & functionality

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/home"
 import Login from "./pages/login"
+import Signup from "./pages/signup";
 import App from "./App";
 
 const routes = [
@@ -9,12 +10,16 @@ const routes = [
         element: <App/>,
         children: [
             {
-                path: '/home',
+                index: '/home',
                 element: <Home />
             },
             {
                 path: '/login',
                 element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
             }
         ]
     },
